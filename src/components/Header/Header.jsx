855c28f4062c7dd8cd7d651081f0ccef9201f8c6
Gollubeff <gollubeff@gmail.com>
2025-10-28 15:37:00 +0300

    до рефакторинга HomePage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import { Link, NavLink } from "react-router-dom";
 import style from "./style.module.css";
 
 function Header() {
+  const getLinkClassName = ({ isActive }) =>
+    `${style.link} ${isActive ? style.active : ""}`;
   return (
     <header className={style.header}>
       <Link to="/" className={style.logo}>
@@ -9,21 +11,11 @@ function Header() {
       </Link>
 
       <nav className={style.nav}>
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? `${style.link} ${style.active}` : style.link
-          }
-        >
+        <NavLink to="/" end className={getLinkClassName}>
           Главная
         </NavLink>
 
-        <NavLink
-          to="/favorites"
-          className={({ isActive }) =>
-            isActive ? `${style.link} ${style.active}` : style.link
-          }
-        >
+        <NavLink to="/favorites" className={getLinkClassName}>
           Избранное
         </NavLink>
       </nav>
